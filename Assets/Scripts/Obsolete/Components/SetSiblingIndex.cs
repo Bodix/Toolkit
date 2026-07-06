0b7d8f75fa65b97bc9d162808e99e554a74a3835
Bodix <bodix321@gmail.com>
2026-07-06 22:45:10 +0300

    Add component

---
+++ Assets/Scripts/Obsolete/Components/SetSiblingIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Obsolete.Components
+{
+	[Obsolete("Used for versions before 2022.1 to move GameObjects up in hierarchy in prefab variants")]
+	public class SetSiblingIndex : MonoBehaviour
+	{
+		[SerializeField]
+		private int _siblingIndex = 0;
+
+		[Button]
+		private void Set()
+		{
+			transform.SetSiblingIndex(_siblingIndex);
+		}
+	}
+}
