b36be157900b26d7cd98116e927a80a50367a17e
Bodix <bodix321@gmail.com>
2026-09-09 01:43:40 +0300

    Improve quests

---
+++ Assets/Packages/Toolkit.Quests/Example/LogReward.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Toolkit.Quests
+{
+	public class LogReward : QuestRewardConfig
+	{
+		public string Reward = "COOL_ITEM";
+
+		public void GrantReward()
+		{
+			Debug.Log("Rewarded " + Reward);
+		}
+	}
+}
