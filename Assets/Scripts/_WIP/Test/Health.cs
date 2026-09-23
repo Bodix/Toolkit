6aae33459fd0e1dd744f46cc3584c96c17470e4d
Bodix <bodix321@gmail.com>
2026-09-23 08:37:59 +0300

    Update submodule

---
+++ Assets/Scripts/_WIP/Test/Health.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+	[SerializeField] private int _max = 100;
+
+	public int Max => _max;
+	public int Current { get; private set; }
+
+	private void Awake() => Current = _max;
+	public void TakeDamage(int amount) => Current = Mathf.Max(Current - amount, 0);
+	public void Heal(int amount) => Current = Mathf.Min(Current + amount, _max);
+}
---
+++ Assets/Scripts/_WIP/Test/TypeSelectorExample.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using PerfectCore.PerfectFoundation;
+using UnityEngine;
+
+// ReSharper disable ArrangeMethodOrOperatorBody
+
+// ReSharper disable CheckNamespace
+
+public interface IEffect
+{
+	void Apply(Health target);
+}
+
+[Serializable]
+public class DamageEffect : IEffect
+{
+	[SerializeField] private int _amount = 10;
+
+	public void Apply(Health target) => target.TakeDamage(_amount);
+}
+
+[Serializable]
+public class HealEffect : IEffect
+{
+	[SerializeField] private int _amount = 10;
+
+	public void Apply(Health target) => target.Heal(_amount);
+}
+
+[Serializable, TypeSelectorName("Heal Effect (% of max)")]
+public class PercentHealEffect : IEffect
+{
+	[SerializeField, Range(0, 1)] private float _percent = 0.25f;
+
+	public void Apply(Health target) =>
+		target.Heal(Mathf.RoundToInt(target.Max * _percent));
+}
+
+public class Potion : MonoBehaviour
+{
+	[SerializeReference, TypeSelector] private IEffect _effect;
+
+	private void OnTriggerEnter(Collider other) =>
+		_effect.Apply(other.GetComponent<Health>());
+}
